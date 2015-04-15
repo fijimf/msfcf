7ab3c91429a886344c41841959e52c34e87ff208
U-Jimonator-9000\Jim <Jim@Jimonator-9000.(none)>
2015-04-14 23:33:14 -0400

    nges in a while

--- msfcf-server/app/controllers/Collateral.scala
+++ msfcf-server/app/controllers/Collateral.scala
@@ -1,10 +1,33 @@
 package controllers
 
+import play.api.libs.json.JsValue
 import play.api.mvc._
-import play.api._
+import play.api.libs.json.Json._
 
 object Collateral extends Controller {
   def index() = Action {
     Ok(views.html.collat())
+  }
+
+  def amortize() = Action(parse.json) { request =>
+    for(
+      originalBalance<-(request.body \ "originalBalance").asOpt[Double];
+      originalTerm<-(request.body \ "originalTerm").asOpt[Int];
+      rate<-(request.body \ "rate").asOpt[Double];
+      originationDate<-(request.body \ "originationDate").asOpt[String];
+      firstPayDate<-(request.body \ "originationDate").asOpt[String]){
+
+    }
+
+
+    maybeDouble.map { dbl =>
+      Ok(toJson(
+        Map("status" -> "OK", "amount" -> ("Hello " + dbl))
+      ))
+    }.getOrElse {
+      BadRequest(toJson(
+        Map("status" -> "KO", "message" -> "Missing parameter [name]")
+      ))
+    }
   }
 }
